4a81ea9426500dca9a103d8ed458185f12344436
Steven Sojka <steven.sojka@toasttab.com>
2022-01-24 07:59:00 -0600

    fix lua queries

--- queries/lua/docs.scm
+++ queries/lua/docs.scm
@@ -1,23 +1,19 @@
 ; Nonlocal functions `function M.test() end`
-(function
-  (function_name) @function.name
-  (parameters
+; Local functions `local function test() end`
+(function_declaration
+  name: (_) @function.name
+  parameters: (parameters
     (identifier)? @function.parameters.name @function.parameters.definition) @function.end_point
   (return_statement)? @function.return) @function.definition
 
-; Local functions `local function test() end`
-(local_function
-  (identifier) @function.name
-  (parameters
-    (identifier)? @function.parameters.name @function.parameters.definition) @function.end_point
-  (return_statement)? @function.return) @function.definition @function.local
-
 ; Non local variables `M.test = {}`
-(local_variable_declaration
-  (variable_declarator
-    (identifier) @variable.name) @variable.end_point) @variable.definition @variable.local
+(assignment_statement
+  (variable_list
+    name: (dot_index_expression
+      field: (identifier) @variable.name)) @variable.end_point) @variable.definition @variable.local
 
 ; Local variables `local test = {}`
 (variable_declaration
-  (variable_declarator
-    (identifier) @variable.name) @variable.end_point) @variable.definition
+  (assignment_statement
+    (variable_list
+      name: (identifier) @variable.name)) @variable.end_point) @variable.definition
